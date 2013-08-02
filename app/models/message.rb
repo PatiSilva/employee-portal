e24f91e931699ec4f22ce292b16e076fce1d0b5f
Patricia Silva <pscorreo@gmail.com>
2013-08-02 14:58:32 -0500

    add additional tests and clean up code

--- app/models/message.rb
+++ app/models/message.rb
@@ -5,4 +5,16 @@ class Message < ActiveRecord::Base
   belongs_to :sender, class_name: 'User', foreign_key: 'sender_id'
 
   scope :unread, -> { where(read: false) }
+
+  # Deliver this message from sender to recipient.
+#
+# sending   - The User sending the message.
+# receiving - The User receiving the message.
+#
+# Returns the delivered Message.
+	def deliver!(sending: nil, receiving: nil)
+		self.sender = sending
+		self.recipient = receiving
+  	raise ArgumentError, "both sender and receiver must be specified" unless sending && receiving
+	end
 end
